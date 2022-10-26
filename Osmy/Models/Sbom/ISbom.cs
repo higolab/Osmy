62b79ff415bbb0cb740cd852fdba03195b6a288a
rioil <rioil.dev@gmail.com>
2022-10-26 17:12:28 +0900

    ファイルを仮追加

---
+++ Osmy/Models/Sbom/ISbom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osmy.Models.Sbom
+{
+    internal interface ISbom
+    {
+        // TODO パッケージの依存関係等を取得できるようにする
+
+        public string Name { get; set; }
+    }
+}
