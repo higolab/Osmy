a89b9724e8a8b0de5e2c1bd427fa63d0b2bd7c32
rioil <rioil.dev@gmail.com>
2023-01-06 15:23:55 +0900

    脆弱性診断結果のアプリ通知を実装

---
+++ Osmy/Models/AppNotificationManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Osmy.Services;
+using System.Linq;
+
+namespace Osmy.Models
+{
+    /// <summary>
+    /// アプリ通知マネージャー
+    /// </summary>
+    /// <see cref="https://learn.microsoft.com/ja-jp/windows/apps/design/shell/tiles-and-notifications/send-local-toast?tabs=desktop"/>
+    internal static class AppNotificationManager
+    {
+        /// <summary>
+        /// 脆弱性通知タグ
+        /// </summary>
+        const string VulnsNotifyTag = nameof(VulnerabilityScanService);
+
+        /// <summary>
+        /// 検出された脆弱性を通知します．
+        /// </summary>
+        public static void NotifyVulnerability()
+        {
+            using var dbContext = new ManagedSoftwareContext();
+            var names = dbContext.ScanResults
+                .Include(x => x.Sbom)
+                .GroupBy(x => x.SbomId)
+                .AsEnumerable()
+                .Select(g => g.OrderByDescending(x => x.Executed).First())
+                .Where(x => x.IsVulnerable)
+                .Select(x => x.Sbom.Name)
+                .ToArray();
+            if (names.Length == 0) { return; }
+
+            var toastBuilder = new ToastContentBuilder();
+            toastBuilder.AddText($"Vulnerabilities detected in {names.Length} software{(names.Length > 1 ? "s" : null)}");
+            toastBuilder.Show(toast => { toast.Tag = VulnsNotifyTag; });
+        }
+    }
+}
