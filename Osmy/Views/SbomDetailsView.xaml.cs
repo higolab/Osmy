0d0ec03b8160eeca8cb8309c99a9fc2a206bddf5
rioil <rioil.dev@gmail.com>
2023-01-18 18:39:19 +0900

    脆弱性情報の表示を改善

--- Osmy/Views/SbomDetailsView.xaml.cs
+++ Osmy/Views/SbomDetailsView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Osmy.Views
 {
@@ -10,6 +16,58 @@ namespace Osmy.Views
         public SbomDetailsView()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            var url = e.Uri.ToString();
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                // ref. https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+    public class UrlToHostConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? sVal = value is string s ? s : value.ToString();
+
+            if (Uri.TryCreate(sVal, UriKind.Absolute, out var uri))
+            {
+                return uri.Host;
+            }
+            else
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
