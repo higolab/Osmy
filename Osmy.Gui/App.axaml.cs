2158734ff2bff45da6235818a4b67023bfce0061
rioil <rioil.dev@gmail.com>
2023-03-10 21:00:43 +0900

    バックグラウンドサービスの一部を移植

--- Osmy.Gui/App.axaml.cs
+++ Osmy.Gui/App.axaml.cs
@@ -2,13 +2,18 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Avalonia.Styling;
+using Osmy.Gui.Models;
 using Osmy.Gui.ViewModels;
 using Osmy.Gui.Views;
+using Osmy.Services;
+using System.Threading.Tasks;
 
 namespace Osmy.Gui
 {
     public partial class App : Application
     {
+        private readonly BackgroundServiceManager _backgroundServiceManager = new();
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -22,11 +27,21 @@ namespace Osmy.Gui
                 {
                     DataContext = new MainWindowViewModel(),
                 };
+
+                _ = Task.Run(StartBackgroundServices);
             }
 
             RequestedThemeVariant = ThemeVariant.Light;
 
             base.OnFrameworkInitializationCompleted();
+        }
+
+        private async Task StartBackgroundServices()
+        {
+            // TODO ìKêÿÇ»èàóùÇ©åüì¢
+            _backgroundServiceManager.Register(new VulnerabilityScanService(new AppNotificationService()));
+            _backgroundServiceManager.Register(new ChecksumVerificationService(new AppNotificationService()));
+            await _backgroundServiceManager.StartAsync().ConfigureAwait(false);
         }
     }
 }
---
+++ Osmy.Gui/Services/BackgroundService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Osmy.Services
+{
+    public abstract class BackgroundService : IDisposable
+    {
+        private CancellationTokenSource? _cancellationTokenSorce;
+        private Task? _executeTask;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cancellationTokenSorce = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+            _executeTask = ExecuteAsync(_cancellationTokenSorce.Token);
+
+            if (_executeTask.IsCompleted)
+            {
+                return _executeTask;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_executeTask is null)
+            {
+                return;
+            }
+
+            try
+            {
+                _cancellationTokenSorce?.Cancel();
+            }
+            finally
+            {
+                await Task.WhenAny(_executeTask, Task.Delay(Timeout.Infinite, cancellationToken)).ConfigureAwait(false);
+            }
+        }
+
+        protected abstract Task ExecuteAsync(CancellationToken stoppingToken);
+
+        public virtual void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
