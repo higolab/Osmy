56e750ea10ed1696066c0cc510080a3e298c1d59
rioil <rioil.dev@gmail.com>
2023-04-19 07:52:30 +0900

    DelegateCommandをファイルに分離

--- Osmy.Gui/ViewModels/SbomListViewViewModel.cs
+++ Osmy.Gui/ViewModels/SbomListViewViewModel.cs
@@ -130,48 +130,4 @@ namespace Osmy.Gui.ViewModels
             return client.GetSboms();
         }
     }
-
-    public class DelegateCommand : ICommand
-    {
-        private readonly Action _action;
-
-        public DelegateCommand(Action action)
-        {
-            _action = action;
-        }
-
-        public event EventHandler? CanExecuteChanged;
-
-        public bool CanExecute(object? parameter)
-        {
-            return true;
-        }
-
-        public void Execute(object? parameter)
-        {
-            _action.Invoke();
-        }
-    }
-
-    public class DelegateCommand<T> : ICommand
-    {
-        private readonly Action<T?> _action;
-
-        public DelegateCommand(Action<T?> action)
-        {
-            _action = action;
-        }
-
-        public event EventHandler? CanExecuteChanged;
-
-        public bool CanExecute(object? parameter)
-        {
-            return true;
-        }
-
-        public void Execute(object? parameter)
-        {
-            _action.Invoke((T?)parameter);
-        }
-    }
 }
